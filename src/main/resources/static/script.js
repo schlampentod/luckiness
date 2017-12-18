4f67e2025824623ec12eb00b541bced8f47d1745
Alex Zalizniak <alex@xplace.com>
2017-12-18 17:06:51 +0200

    Lucky slider

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,6 +6,8 @@ var app = angular.module('luckynes', []);
 app.controller('luckyController', ['$scope', function ($scope) {
     var vm = this;
 
+    vm.luckyBarIndexes = new Array(30);
+
 }]);
 
 
@@ -15,8 +17,32 @@ app.controller('luckyController', ['$scope', function ($scope) {
 app.directive('luckyDirective', ['$interval', 'dateFilter', function ($interval, dateFilter) {
 
     function link(scope, element, attrs) {
-        var index = attrs["luckyDirective"];
-        debugger;
+
+        var index = parseInt(attrs["luckyDirective"]);
+
+        console.info("Created: " + index);
+
+        scope.luckyBarId = "lucky_bar_" + index;
+        scope.luckySliderId = "lucky_slider_" + index;
+
+        element.mousemove(function (evt) {
+
+            var offset  = evt.offsetX;
+            var sliderElem = getLuckySliderElem();
+
+            console.info("Moved: " + index + " to " + offset);
+
+            sliderElem.css("margin-left", offset + "px");
+        });
+
+        element.click(function (evt) {
+            console.info("Clicked: " + index);
+        });
+
+        function getLuckySliderElem() {
+            var luckySliderElem = $("#" + scope.luckySliderId);
+            return luckySliderElem;
+        }
     }
 
     return {
