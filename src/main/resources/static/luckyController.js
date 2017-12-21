44e9856512abd68e6b23b2efe707c442cee40336
Alex Zalizniak <alex@xplace.com>
2017-12-21 14:13:00 +0200

    Misc fixes

--- src/main/resources/static/luckyController.js
+++ src/main/resources/static/luckyController.js
@@ -2,12 +2,7 @@
  *
  */
 
-app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$interval','$http','$translate', function ($scope, luckyService, luckyFactory, $interval, $http, $translate) {
-
-    $scope.changeLanguage = function(lang){
-        $translate.use(lang);
-    }
-
+app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', function ($scope, luckyService, luckyFactory, $interval, $http, $translate) {
 
     var vm = this;
 
@@ -21,15 +16,11 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
     };
 
     vm.luckyBinchBarsOffsets = [];
-
-    populateBarOffsets();
-
-    vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
-
+    vm.luckyBarSumValue = "";
     vm.luckyProvidedExactValue = "";
 
     $scope.$watch(function () {//по изменению массива расположений баров генерить "Key" и "Address"
-            return vm.luckyBinchBarsOffsets;
+        return vm.luckyBinchBarsOffsets;
     }, function (newVal, oldVal) {
         luckyService.currentChooser.setChooserBarsOffsets(vm.luckyBinchBarsOffsets);
         vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
@@ -40,9 +31,9 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
     }, function (newVal, oldVal) {
         vm.luckyCtrlPma.luckyTotalGeneratedKeys++;
         luckyService.checkKeyInBlockChain(newVal);
-        var promise = $http.get('http://localhost:8080/rest/v1/lucky/addresses/'+vm.luckyBarSumValue);
+        var promise = $http.get('http://localhost:8080/rest/v1/lucky/addresses/' + vm.luckyBarSumValue);
         promise.then(function (response) {
-            vm.luckyCtrlDerivedKeys.luckyKeyPublicAddress=response.data.publicAddressAsHex;
+            vm.luckyCtrlDerivedKeys.luckyKeyPublicAddress = response.data.publicAddressAsHex;
         });
     });
 
@@ -51,38 +42,37 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
     }, function (newVal, oldVal) {
         var parsedOffsets = luckyService.currentChooser.setProvidedChosenStringValue(vm.luckyProvidedExactValue);
         luckyService.currentChooser.generateKeysOfValue(parsedOffsets);
-        populateBarOffsets();
+        readBinchStatus();
     });
 
     $scope.getkeys = function (event) {
-        var one = bigInt("1");
-        var bigValue = bigInt(vm.luckyProvidedExactValue);
+
         if (event.keyCode == 38) {
-            bigValue = bigValue.add(one);
-            vm.luckyProvidedExactValue = bigValue.toString();
-        }
-        if (event.keyCode == 40) {
-            bigValue = bigValue.subtract(one);
-            vm.luckyProvidedExactValue = bigValue.toString();
+            luckyService.currentChooser.incrementChosenValue();
+            readBinchStatus();
+        } else if (event.keyCode == 40) {
+            luckyService.currentChooser.decrementChosenValue();
+            readBinchStatus();
         }
     };
 
+    $scope.changeLanguage = function (lang) {
+        $translate.use(lang);
+    };
+
     vm.onParseProvidedValueClick = function () {//по клику сгениреть масив расположений баров на хтмл
-        var parsedOffsets = luckyService.currentChooser.setProvidedChosenStringValue(vm.luckyProvidedExactValue);
-        luckyService.currentChooser.generateKeysOfValue(parsedOffsets);
-        populateBarOffsets();
+        luckyService.currentChooser.setProvidedChosenStringValue(vm.luckyProvidedExactValue);
+        readBinchStatus();
     };
 
     vm.onGenerateMinClick = function () {
         luckyService.currentChooser.generateMinValue();
-        populateBarOffsets();
-        vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
+        readBinchStatus();
     };
 
     vm.onGenerateMaxClick = function () {
         luckyService.currentChooser.generateMaxValue();
-        populateBarOffsets();
-        vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
+        readBinchStatus();
     };
 
     vm.onGenerateRandomClick = function () {
@@ -106,16 +96,20 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
 
     function generateRandomOffsets() {
         luckyService.currentChooser.generateRandomBarOffsets();
-        populateBarOffsets();
-        vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
+        readBinchStatus();
     }
 
-    function populateBarOffsets() {
+    function readBinchStatus() {
+
         vm.luckyBinchBarsOffsets = [];
         _.forEach(luckyService.currentChooser.binchBars, function (bb, i) {
             vm.luckyBinchBarsOffsets.push(bb.binchBarOffsetPx);
         });
+
+        vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
     }
+
+    readBinchStatus();
 
 }]);
 
