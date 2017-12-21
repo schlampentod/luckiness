299719e2e6cc7973d27d38b3213c04c715da0788
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-21 17:01:38 +0200

    Range css fix

--- src/main/resources/static/app_config.js
+++ src/main/resources/static/app_config.js
@@ -1,33 +1,15 @@
 app.config(['$translateProvider',function($translateProvider){
 
-    var en_translations = {
-        "Key" : "Key",
-        "totCheck" : "Total checked",
-        "Address" : "Address",
-        "Start" : "Start",
-        "Stop" : "Stop",
-        "Parse": "Parse",
-        "Min" : "Min",
-        "Max" : "Max",
-        "Random" : "Random"
-    };
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'resources_',
+        suffix: '.json'
+    });
 
-    var ua_translations = {
-        "Key" : "Ключ",
-        "totCheck" : "Усього зареєстровано",
-        "Address" : "Address",
-        "Start" : "Старт",
-        "Stop" : "Стоп",
-        "Parse": "Вставить",
-        "Min" : "Мінімальний",
-        "Max" : "Максимальний",
-        "Random" : "Рандом"
-    };
+    //$translateProvider.translations('en',en_translations);
 
-    $translateProvider.translations('en',en_translations);
-
-    $translateProvider.translations('ua',ua_translations);
+    //$translateProvider.translations('ua',ua_translations);
 
     $translateProvider.preferredLanguage('en');
 
 }]);
+
