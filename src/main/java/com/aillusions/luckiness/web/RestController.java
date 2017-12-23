22344b3c6e3ebcebd22775578b7abfbb59bbcc76
aillusions <aillusions@gmail.com>
2017-12-23 20:02:34 +0200

    Known keys provider

--- src/main/java/com/aillusions/luckiness/web/RestController.java
+++ src/main/java/com/aillusions/luckiness/web/RestController.java
@@ -3,6 +3,7 @@ package com.aillusions.luckiness.web;
 import com.aillusions.luckiness.AsyncService;
 import com.aillusions.luckiness.DormantAddressProvider;
 import com.aillusions.luckiness.DormantBloomFilter;
+import com.aillusions.luckiness.KnownKeysProvider;
 import lombok.Setter;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.bitcoinj.core.*;
@@ -94,63 +95,10 @@ public class RestController {
 
         KnownKeysDto rv = new KnownKeysDto();
 
-    /*
-        http://localhost:8080/rest/v1/lucky/convert/base68/5JApJM8iAcxJ3JgW5oiRVKNrvpmqvqzSvkaEBC7cEoFvSE8h1rN
-    */
+        for (String s : KnownKeysProvider.getKnownKeys()) {
+            rv.getKnownKeyDtos().add(new KnownKeyDto(s));
+        }
 
-        rv.getKnownKeyDtos().add(new KnownKeyDto("1"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("2"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("3"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("4"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("5"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("6"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("7"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("8"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("10"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("11"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("12"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("14"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("15"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("16"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("18"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("19"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("21"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("22"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("23"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("24"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("25"));
-
-        rv.getKnownKeyDtos().add(new KnownKeyDto("48635463943209834798109814161294753926839975257569795305637098542720658922315"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("85373582762808404920801888792437094602169475096082456154754419692323304989563"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("43388321209941149759420236104888244958223766953174235657296806338137402595305"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("96094161643976066833367867971426158458230048495430276217795328666133331159861"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("104920238006865337205013407090248200170018306865343388364051008767965015414403"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("91634880152443617534842621287039938041581081254914058002978601050179556493499"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("16818733035250147179836346549208023004715419562600096834104913104569384956177"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("9296995843462117880257900402549316557588719833659989225024886941245434927424"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("5232581527863235096587321474292674180630556774295345035650804382406877176825"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("52142063543217935108392605932536905068334213121597159393376024684286053089353"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("84910071079903469711540322182995519010752598753241787550350295165653577042301"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("26620332071894918181543005365761258030749426457641997779997397538985897748066"));
-
-        rv.getKnownKeyDtos().add(new KnownKeyDto("86385075817194309241889933189838769976076542292920476979308177169247389148514"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("54135252281593754928977010706017271185350777762748401427924688480919409292366"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("7719472615821079694904732333912527190217998977709370935963838933860875309329"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("80469823408774753511317230000790051071390039989872278573322755840844005128873"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("48639094777952439900997650962019858887708497656787781882390683584059677479035"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("21084577330837739369726464359992438658490297576745957612705280406384195231572"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("16206629509484900278185934256394058544247806074329441863030135040696487329819"));
-
-        rv.getKnownKeyDtos().add(new KnownKeyDto("77194726158210796949047323339125271902179989777093709359638389338608753093290"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("128"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("105"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("97"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("81"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("66"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("98675694130411510693845893181359175902614576286020627467498811268541903893103"));
-        rv.getKnownKeyDtos().add(new KnownKeyDto("68084331379661139878044811113493142426362492056136683088026367871757575059"));
-
-        
         Collections.sort(rv.getKnownKeyDtos(), new Comparator<KnownKeyDto>() {
             @Override
             public int compare(KnownKeyDto o1, KnownKeyDto o2) {
