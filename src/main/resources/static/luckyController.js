0870576067adaca6b7c75a7134db0581bc773acc
Grigory <grigirij.zalizniak.salazar@gmail.com>
2017-12-24 20:15:16 +0200

    Range css fix

--- src/main/resources/static/luckyController.js
+++ src/main/resources/static/luckyController.js
@@ -13,75 +13,23 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
 
     vm.luckyCtrlDerivedKeys = {
         luckyKeyPublicAddress: null,
-        luckyKeyPublicAddressBalance: 0,
-        luckyKeyPrivateKeyAsWIF: null,
-        luckyKeyWasFound: false
+        luckyKeyPrivateKeyAsWIF: null
     };
 
-    vm.luckyBarsSameOffset = 0;
     vm.luckyBinchBarsOffsets = [];
     vm.luckyBarSumValue = "";
 
-    $scope.selectedKnownKey = null;
+    $scope.selectedMagicKeys = null;
 
-    $scope.$watch(function () {
-        return vm.luckyBarsSameOffset;
-    }, function (newVal, oldVal) {
-
-        if (newVal) {
-            _.forEach(vm.luckyBinchBarsOffsets, function (barOffset, i) {
-                vm.luckyBinchBarsOffsets[i] = parseInt(newVal);
-            })
-        }
-    });
-
-    $scope.getkeySluckyBarsSameOffset = function (event) {
-
-        if (event.keyCode == 38) {
-            vm.luckyBarsSameOffset = parseInt(vm.luckyBarsSameOffset) + 1;
-        } else if (event.keyCode == 40) {
-            vm.luckyBarsSameOffset = parseInt(vm.luckyBarsSameOffset) - 1;
-        }
-    };
-
-    var allKeyIsPressed = false;
-    $(document).keyup(function (evt) {
-        if (evt.keyCode == 65) {
-            allKeyIsPressed = false;
-        }
-    }).keydown(function (evt) {
-        if (evt.keyCode == 65) {
-            allKeyIsPressed = true;
-            // console.log('A')
-        }
-    });
-
-    $scope.funcXyjank = function () {
-        vm.luckyBarSumValue = $scope.selectedKnownKey.knownKeyDecimal;
+    $scope.funcXyjank = function () {//передать некоторые маленькие ебучие данные, в ебучее текстовое поле
+        vm.luckyBarSumValue = $scope.selectedMagicKeys.knownKeyDecimal;//$("#myselect option:selected").text();
     };
 
     $scope.$watch(function () { // по изменению массива расположений баров генерить "Key" и "Address"
         return vm.luckyBinchBarsOffsets;
     }, function (newVal, oldVal) {
-
-        if (allKeyIsPressed) {
-
-            var changeIdxes = findChangedBarIndexes(newVal, oldVal);
-            if (changeIdxes.length === 1) {
-                var changedIdx = changeIdxes[0];
-                var changeDelta = newVal[changedIdx] - oldVal[changedIdx];
-
-                _.forEach(vm.luckyBinchBarsOffsets, function (barOffset, i) {
-                    vm.luckyBinchBarsOffsets[i] = vm.luckyBinchBarsOffsets[i] + changeDelta;
-                });
-
-                return;
-            }
-        }
-
         luckyService.currentChooser.setChooserBarsOffsets(vm.luckyBinchBarsOffsets);
         readBinchStatus();
-
     }, true);
 
     $scope.$watch(function () { // наблюдаем и отправляем запросики, и забиваем Address-суличку..
@@ -91,29 +39,24 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
         if (vm.luckyBarSumValue !== luckyService.currentChooser.chosenValue.toString(10)) {
             luckyService.currentChooser.setProvidedChosenStringValue(vm.luckyBarSumValue);
             readBinchStatus();
-            //return;
+            return;
         }
 
         vm.luckyCtrlPma.luckyTotalGeneratedKeys++;
 
+        addressAnalyticsService.checkKeyInBlockChain(newVal).then(function (CheckKeyResultDto) {
+            if (CheckKeyResultDto['checkedKeyFound']) {
+                alert("Found: " + newVal);
+            }
+        }, function (errors) {
+            console.error(errors);
+        });
+
         addressAnalyticsService.resolveBitcoinAddressByKey(vm.luckyBarSumValue).then(function (AddressesResultDto) {
             vm.luckyCtrlDerivedKeys.luckyKeyPublicAddress = AddressesResultDto['publicAddressAsHex'];
             vm.luckyCtrlDerivedKeys.luckyKeyPrivateKeyAsWIF = AddressesResultDto['privateKeyAsWIF'];
-
-            addressAnalyticsService.checkKeyInBlockChain(newVal).then(function (CheckKeyResultDto) {
-                vm.luckyCtrlDerivedKeys.luckyKeyWasFound = ( _.includes(CheckKeyResultDto['checkedKeysMatched'], newVal) && CheckKeyResultDto['checkedKeyFound']);
-
-                if (vm.luckyCtrlDerivedKeys.luckyKeyWasFound) {
-                    addressAnalyticsService.getAddressBalance(vm.luckyCtrlDerivedKeys.luckyKeyPublicAddress).then(function (balance) {
-                        vm.luckyCtrlDerivedKeys.luckyKeyPublicAddressBalance = balance / 100000000;
-                        console.info("Balance: of " + vm.luckyCtrlDerivedKeys.luckyKeyPublicAddress + " is " + balance);
-                    });
-                }
-
-            }, function (errors) {
-                console.error(errors);
-            });
         });
+
     });
 
 
@@ -134,24 +77,25 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
             readBinchStatus();
         }
     };
+    $scope.changeOffSets = function (event, index, obj) {//изменять на еденицу значение офсетов и соответственно рассположения баров
 
-    $scope.changeOffSets = function (event, index) {//изменять на еденицу значение офсетов и соответственно рассположения баров
 
         if (event.keyCode == 38) {
             if (vm.luckyBinchBarsOffsets[index] <= 999) {
                 vm.luckyBinchBarsOffsets[index]++;
                 luckyService.currentChooser.setChooserBarsOffsets(vm.luckyBinchBarsOffsets);
-                console.log(vm.luckyBinchBarsOffsets[index]);
                 readBinchStatus();
             }
+            ;
 
         } else if (event.keyCode == 40) {
             if (vm.luckyBinchBarsOffsets[index] >= 1) {
                 vm.luckyBinchBarsOffsets[index]--;
                 luckyService.currentChooser.setChooserBarsOffsets(vm.luckyBinchBarsOffsets);
-                console.log(vm.luckyBinchBarsOffsets[index]);
                 readBinchStatus();
             }
+            ;
+
         }
 
     };
@@ -205,18 +149,6 @@ app.controller('luckyController', ['$scope', 'luckyService', 'luckyFactory', '$i
     function readBinchStatus() {
         vm.luckyBinchBarsOffsets = luckyService.currentChooser.getBinchBarOffsets();
         vm.luckyBarSumValue = luckyService.currentChooser.chosenValue.toString(10);
-    }
-
-    function findChangedBarIndexes(newVal, oldVal) {
-        var rv = [];
-
-        _.forEach(newVal, function (barOffset, i) {
-            if (newVal[i] != oldVal[i]) {
-                rv.push(i);
-            }
-        });
-
-        return rv;
     }
 
     readBinchStatus();
