b3d7a46f0b1a3576632becc9fae4d9db7ab2e732
Alex Zalizniak <alex@xplace.com>
2017-12-25 13:05:45 +0200

    Seq gen - wip

--- src/main/resources/static/script/sequenceGeneratorController.js
+++ src/main/resources/static/script/sequenceGeneratorController.js
@@ -5,7 +5,37 @@ app.controller('sequenceGeneratorController', ['$scope', 'luckyService', 'luckyF
 
     var vm = this;
 
+    var MIN_NUMBER = luckyService.currentChooser.MIN_BIG_NUMBER;
+    var MAX_NUMBER = luckyService.currentChooser.MAX_BIG_NUMBER;
+
+    vm.KeySequenceGenerationStrategy = {
+        CONCAT_STRATEGY: "CONCAT_STRATEGY",
+        SUMMATION_STRATEGY: "SUMMATION_STRATEGY",
+        MULTIPLICATION_STRATEGY: "MULTIPLICATION_STRATEGY"
+    };
+
+    vm.allGenerationStrategies = [];
+    _.forOwn(vm.KeySequenceGenerationStrategy, function (value, key) {
+        if (typeof value === "string") {
+            vm.allGenerationStrategies.push(key);
+        }
+    });
+
+    vm.generationSequenceStrategy = vm.KeySequenceGenerationStrategy.CONCAT_STRATEGY;
+    vm.generationSequenceTemplate = "";
     vm.generatedKeysSequence = [];
+
+    vm.onGenerateNewSequence = function () {
+
+    };
+
+    vm.onTryAllGeneratedSequence = function () {
+
+    };
+
+    vm.onTryGeneratedSequenceElement = function (element) {
+
+    };
 
 }]);
 
