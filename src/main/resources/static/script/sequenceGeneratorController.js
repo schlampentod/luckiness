000070cf0617463ab2228075867ef31668c28f24
Alex Zalizniak <alex@xplace.com>
2017-12-25 12:54:13 +0200

    Sequence generator

---
+++ src/main/resources/static/script/sequenceGeneratorController.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+app.controller('sequenceGeneratorController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout) {
+
+    var vm = this;
+
+    vm.generatedKeysSequence = [];
+
+}]);
+
