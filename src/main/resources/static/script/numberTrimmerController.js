1b2a13fc44df813a44ae8e4f4ea8334e6f894ec1
Alex Zalizniak <alex@xplace.com>
2017-12-25 18:57:05 +0200

    Number trimmer

--- src/main/resources/static/script/numberTrimmerController.js
+++ src/main/resources/static/script/numberTrimmerController.js
@@ -5,7 +5,49 @@ app.controller('numberTrimmerController', ['$scope', 'luckyService', 'luckyFacto
 
     var vm = this;
 
+    var MAX_NUMBER = luckyService.currentChooser.MAX_BIG_NUMBER;
+
+
     vm.luckyNumberTrimmerToggled = false;
+
+    vm.trimmingNumberValue = null;
+
+    $scope.$watch(function () {
+        return vm.trimmingNumberValue
+    }, function (newVal, oldVal) {
+        onTrimmingNumberChanged(vm.trimmingNumberValue);
+    });
+
+    //
+    //
+    //
+
+    function onTrimmingNumberChanged(newValue) {
+
+        if (!newValue || newValue === "") {
+            return;
+        }
+
+        var keysToCheck = [];
+
+        var newValueBig = bigInt(newValue, 10);
+
+        if (newValueBig.lesserOrEquals(MAX_NUMBER)) {
+            keysToCheck.push(newValue);
+        } else {
+
+            var newValueCut = newValue;
+            var newValueBigCut = bigInt(newValueCut);
+            while (MAX_NUMBER.lesserOrEquals(newValueBigCut)) {
+                newValueCut = newValueCut.substring(1);
+                newValueBigCut = bigInt(newValueCut);
+            }
+
+            keysToCheck.push(newValueCut);
+        }
+
+        $scope.$emit(luckyConstants.TRY_KEYS_SEQUENCE_EVT, {keysArrayToTry: keysToCheck});
+    }
 
 }]);
 
