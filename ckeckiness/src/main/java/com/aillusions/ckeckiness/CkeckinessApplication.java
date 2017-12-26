c74b010a7aef9411a58c9bef6c59b1a963be4476
Alex Zalizniak <alex@xplace.com>
2017-12-26 15:21:12 +0200

    Checkiness - added

---
+++ ckeckiness/src/main/java/com/aillusions/ckeckiness/CkeckinessApplication.java
@@ -0,0 +1,12 @@
+package com.aillusions.ckeckiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkeckinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkeckinessApplication.class, args);
+	}
+}
