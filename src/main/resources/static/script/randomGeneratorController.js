261067e0343944fc19aedbfb0619c4eabb638e0c
Alex Zalizniak <alex@xplace.com>
2017-12-26 18:38:06 +0200

    Random block

---
+++ src/main/resources/static/script/randomGeneratorController.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+app.controller('randomGeneratorController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', 'luckyConstants', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout, luckyConstants) {
+
+    var vm = this;
+
+    vm.luckyRandomFormToggled = false;
+
+}]);
+
