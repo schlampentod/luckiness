44e958fd926fd2d3a076522930b1348dc70f84e0
Alex Zalizniak <alex@xplace.com>
2018-01-02 00:28:54 +0200

    Digits - wip

---
+++ src/main/resources/static/script/digitsGeneratorController.js
@@ -0,0 +1,12 @@
+/**
+ *
+ */
+app.controller('digitsGeneratorController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', 'luckyConstants', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout, luckyConstants) {
+
+    var vm = this;
+
+
+    vm.luckyDigitsFormToggled = false;
+
+}]);
+
