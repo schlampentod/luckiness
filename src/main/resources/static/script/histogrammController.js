98b0a7d12bea2269b4d0fec7cd48a1d99cb099b9
Alex Zalizniak <alex@xplace.com>
2018-01-02 13:15:24 +0200

    histogramm - wip

--- src/main/resources/static/script/histogrammController.js
+++ src/main/resources/static/script/histogrammController.js
@@ -7,5 +7,21 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
 
     vm.histogrammFormToggled = true;
 
+    vm.histogrammValues = new Array(10);
+    vm.histogrammValues.fill(0);
+
+    $scope.$on(luckyConstants.KEY_VALUE_CHANGED_EVT, function (event, args) {
+
+        var newKeyVal = args.newChosenKey;
+
+        vm.histogrammValues.fill(0);
+
+        for (var i = 0; i < newKeyVal.length; i++) {
+            var charAtI = parseInt(newKeyVal.charAt(i));
+            vm.histogrammValues[charAtI] = vm.histogrammValues[charAtI] + 1;
+        }
+
+    });
+
 }]);
 
