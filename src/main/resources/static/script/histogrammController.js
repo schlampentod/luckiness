c56a9f6f13e49fadfc81e34c39cb397ed06a769c
Alex Zalizniak <alex@xplace.com>
2018-01-02 13:02:54 +0200

    histogramm - wip

---
+++ src/main/resources/static/script/histogrammController.js
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', 'luckyConstants', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout, luckyConstants) {
+
+    var vm = this;
+
+    vm.histogrammFormToggled = true;
+
+}]);
+
