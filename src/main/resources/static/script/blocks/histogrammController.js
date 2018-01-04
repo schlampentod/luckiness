a87cce44fc11d5aa5cb32ac483e9cb9d595a7516
Alex Zalizniak <alex@xplace.com>
2018-01-05 00:54:37 +0200

    Histo

--- src/main/resources/static/script/blocks/histogrammController.js
+++ src/main/resources/static/script/blocks/histogrammController.js
@@ -6,9 +6,13 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
     var vm = this;
 
     vm.histogrammLabels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    vm.histogrammLabelsBin = ["0", "1"];
 
     vm.histogrammValues = new Array(10);
     vm.histogrammValues.fill(10);
+
+    vm.histogrammValuesBin = new Array(2);
+    vm.histogrammValuesBin.fill(1);
 
     vm.histoChartOptions = {
         legend: {
@@ -29,6 +33,25 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
         }
     };
 
+    vm.histoChartOptionsBin = {
+        legend: {
+            display: false
+        },
+        animation: {
+            duration: 1
+        },
+        tooltips: {
+            enabled: true,
+            mode: 'single',
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    var label = vm.histogrammLabelsBin[tooltipItem.index];
+                    return label;
+                }
+            }
+        }
+    };
+
     $scope.$on(luckyConstants.KEY_VALUE_CHANGED_EVT, function (event, args) {
 
         var newKeyVal = args.newChosenKey;
@@ -38,6 +61,14 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
         for (var i = 0; i < newKeyVal.length; i++) {
             var charAtI = parseInt(newKeyVal.charAt(i));
             vm.histogrammValues[charAtI] = vm.histogrammValues[charAtI] + 1;
+        }
+
+        vm.histogrammValuesBin.fill(0);
+
+        var binValStr = bigInt(newKeyVal).toString(2);
+        for (var i = 0; i < binValStr.length; i++) {
+            var charAtI = parseInt(binValStr.charAt(i));
+            vm.histogrammValuesBin[charAtI] = vm.histogrammValuesBin[charAtI] + 1;
         }
 
     });
