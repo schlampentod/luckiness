8e7691c91091b846ebb2c1173e800f5e7c27de0d
Alex Zalizniak <alex@xplace.com>
2018-01-04 17:24:27 +0200

    Tabs ctrl

---
+++ src/main/resources/static/script/generationTabsController.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+app.controller('generationTabsController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout) {
+
+    var vm = this;
+
+
+}]);
+
