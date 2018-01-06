1f9f2993a757d00f703621f960ae40a8b9e2c534
Alex Zalizniak <alex@xplace.com>
2018-01-06 12:47:14 +0200

    Cleanup

--- src/main/resources/static/script/histogramController.js
+++ src/main/resources/static/script/histogramController.js
@@ -1,18 +1,18 @@
 /**
  *
  */
-app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', 'luckyConstants', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout, luckyConstants) {
+app.controller('histogramController', ['$scope', 'luckyService', 'luckyFactory', '$interval', '$http', '$translate', 'addressAnalyticsService', '$window', 'keyGenerationService', '$timeout', 'luckyConstants', function ($scope, luckyService, luckyFactory, $interval, $http, $translate, addressAnalyticsService, $window, keyGenerationService, $timeout, luckyConstants) {
 
     var vm = this;
 
-    vm.histogrammLabels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    vm.histogrammLabelsBin = ["0", "1"];
+    vm.histogramLabels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    vm.histogramLabelsBin = ["0", "1"];
 
-    vm.histogrammValues = new Array(10);
-    vm.histogrammValues.fill(10);
+    vm.histogramValues = new Array(10);
+    vm.histogramValues.fill(10);
 
-    vm.histogrammValuesBin = new Array(2);
-    vm.histogrammValuesBin.fill(1);
+    vm.histogramValuesBin = new Array(2);
+    vm.histogramValuesBin.fill(1);
 
     vm.histoChartOptions = {
         legend: {
@@ -26,7 +26,7 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
             mode: 'single',
             callbacks: {
                 label: function(tooltipItem, data) {
-                    var label = vm.histogrammLabels[tooltipItem.index];
+                    var label = vm.histogramLabels[tooltipItem.index];
                     return label;
                 }
             }
@@ -45,7 +45,7 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
             mode: 'single',
             callbacks: {
                 label: function(tooltipItem, data) {
-                    var label = vm.histogrammLabelsBin[tooltipItem.index];
+                    var label = vm.histogramLabelsBin[tooltipItem.index];
                     return label;
                 }
             }
@@ -56,19 +56,19 @@ app.controller('histogrammController', ['$scope', 'luckyService', 'luckyFactory'
 
         var newKeyVal = args.newChosenKey;
 
-        vm.histogrammValues.fill(0);
+        vm.histogramValues.fill(0);
 
         for (var i = 0; i < newKeyVal.length; i++) {
             var charAtI = parseInt(newKeyVal.charAt(i));
-            vm.histogrammValues[charAtI] = vm.histogrammValues[charAtI] + 1;
+            vm.histogramValues[charAtI] = vm.histogramValues[charAtI] + 1;
         }
 
-        vm.histogrammValuesBin.fill(0);
+        vm.histogramValuesBin.fill(0);
 
         var binValStr = bigInt(newKeyVal).toString(2);
         for (var i = 0; i < binValStr.length; i++) {
             var charAtI = parseInt(binValStr.charAt(i));
-            vm.histogrammValuesBin[charAtI] = vm.histogrammValuesBin[charAtI] + 1;
+            vm.histogramValuesBin[charAtI] = vm.histogramValuesBin[charAtI] + 1;
         }
 
     });
