6c48a04f6386a0e4060ee138a128c862afee2862
Alex Zalizniak <alex@xplace.com>
2018-01-09 17:23:21 +0200

    Carving

--- src/main/resources/static/script/blocks/carvingController.js
+++ src/main/resources/static/script/blocks/carvingController.js
@@ -5,5 +5,55 @@ app.controller('carvingController', ['$scope', 'luckyService', 'luckyFactory', '
 
     var vm = this;
 
+    var MAX_NUMBER = luckyService.currentChooser.MAX_BIG_NUMBER;
+
+    vm.maxNumStrBin = MAX_NUMBER.toString(2);
+
+    var valueLength = vm.maxNumStrBin.length; // 256 bits
+    var lineLength = 32; // 32 == 256 / 8;
+
+    vm.maxNumStrBinLines = new Array(8);
+    vm.digitGeneratorPressedKeyDigit = null;
+
+    init();
+
+
+    //
+    //
+    //
+
+    function init() {
+
+        _.forEach(vm.maxNumStrBinLines, function (line, lineIdx) {
+            vm.maxNumStrBinLines[lineIdx] = new Array(lineLength);
+            var lineArray = vm.maxNumStrBinLines[lineIdx]
+
+            _.forEach(lineArray, function (elem, elemIdx) {
+                var charIdx = (lineIdx * lineLength) + elemIdx;
+                var char = vm.maxNumStrBin.charAt(charIdx);
+                lineArray[elemIdx] = char;
+            });
+        });
+
+        $(document).keypress(function (evt) {
+            var charCode = evt.which || evt.keyCode;
+            var char = String.fromCharCode(charCode);
+
+            if (/[0-1]/g.test(char)) {
+                vm.digitGeneratorPressedKeyDigit = parseInt(char);
+            } else {
+                vm.digitGeneratorPressedKeyDigit = null;
+            }
+
+            console.info("onDigitUpIncrement: " + vm.digitGeneratorPressedKeyDigit);
+            $timeout($scope.$apply());
+        });
+
+        $(document).keyup(function (evt) {
+            vm.digitGeneratorPressedKeyDigit = null;
+            $timeout($scope.$apply());
+        });
+    }
+
 }]);
 
