3ebf87382d7c8901df789a1f4b3c78f501e3bbc5
Alex Zalizniak <alex@xplace.com>
2018-01-09 17:49:19 +0200

    Carving

--- src/main/resources/static/script/blocks/carvingController.js
+++ src/main/resources/static/script/blocks/carvingController.js
@@ -6,8 +6,10 @@ app.controller('carvingController', ['$scope', 'luckyService', 'luckyFactory', '
     var vm = this;
 
     var MAX_NUMBER = luckyService.currentChooser.MAX_BIG_NUMBER;
+    var BOARD_INITIAL_NUMBER = bigInt("57953998617275373084870362867211165091510292431969421612915497067826907655000");
 
     vm.maxNumStrBin = MAX_NUMBER.toString(2);
+    vm.initialStrBin = BOARD_INITIAL_NUMBER.toString(2);
 
     var valueLength = vm.maxNumStrBin.length; // 256 bits
     var lineLength = 32; // 32 == 256 / 8;
@@ -31,8 +33,16 @@ app.controller('carvingController', ['$scope', 'luckyService', 'luckyFactory', '
     //
     //
 
-    function getFinalNumber() {
+    function getFinalNumberBin() {
+        var rv = "";
+        _.forEach(vm.maxNumStrBinLines, function (lineArray, lineIdx) {
+            _.forEach(lineArray, function (elem, elemIdx) {
+                rv += elem;
+            });
+        });
 
+        // console.info("getFinalNumber: " + rv);
+        return rv;
     }
 
     function init() {
@@ -42,7 +52,16 @@ app.controller('carvingController', ['$scope', 'luckyService', 'luckyFactory', '
         $scope.$watch(function () {
             return vm.maxNumStrBinLines;
         }, function (newVal, oldVal) {
-            getFinalNumber();
+           var finalNumBin =  getFinalNumberBin();
+           var bn = bigInt(finalNumBin, 2);
+           if(bn.lesserOrEquals(MAX_NUMBER)) {
+               var bnDecStr = bn.toString(10);
+               console.info("carved to: " + bnDecStr);
+               $scope.$emit(luckyConstants.TRY_KEYS_SEQUENCE_EVT, {keysArrayToTry: [bnDecStr]});
+           }else{
+               initCarvingBoard();
+           }
+
         }, true);
 
         $(document).keypress(function (evt) {
@@ -68,13 +87,16 @@ app.controller('carvingController', ['$scope', 'luckyService', 'luckyFactory', '
 
     function initCarvingBoard() {
 
+        console.info("Resetting carving board.");
+
         _.forEach(vm.maxNumStrBinLines, function (line, lineIdx) {
             vm.maxNumStrBinLines[lineIdx] = new Array(lineLength);
             var lineArray = vm.maxNumStrBinLines[lineIdx];
 
             _.forEach(lineArray, function (elem, elemIdx) {
                 var charIdx = (lineIdx * lineLength) + elemIdx;
-                var char = vm.maxNumStrBin.charAt(charIdx);
+                //var char = vm.maxNumStrBin.charAt(charIdx);
+                var char = vm.initialStrBin.charAt(charIdx);
                 lineArray[elemIdx] = char;
             });
         });
